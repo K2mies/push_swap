471020720f22971afba3c5016d87fc5d2375a891
Ross Hvidsten <rhvidste@student.hive.fi>
2024-12-18 16:55:20 +0200

    todays starting point

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,11 +6,14 @@
 /*   By: rhvidste <rhvidste@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 13:04:29 by rhvidste          #+#    #+#             */
-/*   Updated: 2024/12/18 13:53:23 by rhvidste         ###   ########.fr       */
+/*   Updated: 2024/12/18 16:55:00 by rhvidste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+char	*ft_strncpy(char *s1, char *s2, int n);
+char	**ft_split(char *str);
 
 int	main(int argc, char **argv)
 {
@@ -24,6 +27,56 @@ int	main(int argc, char **argv)
 		return (1);
 	else if	(argc == 2)
 	{
+		//parse the string if it is a string of characters, convert it into an array of inputs. 
 		argv = ft_split(argv[1], ' ')
 	}
+}
+
+char	*ft_strncpy(char *s1, char *s2, int n)
+{
+	int		i;
+	i = -1;
+	while (i++ < n && s2[i])
+		s1[i] = s2[i];
+	s1[i] = '\0';
+	return (s1);
+}
+char	**ft_split(char *str)
+{
+	int		i;
+	int		j;
+	int		k;
+	int		wc;
+
+	i = 0;
+	j = 0;
+	k = 0;
+	wc = 0;
+
+	while (str[i])
+	{
+		while (str[i] && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'))
+			i++;
+		if (str[i])
+			wc++;
+		while (str[i] && (str[i] != ' ' && str[i] != '\t' && str[i] != '\n'))
+			i++;
+	}
+	char **out = (char **)malloc(sizeof(char *) * (wc + 1))
+	i = 0;
+	while (str[i])
+	{	
+		while (str[i] && (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'))
+			i++;
+		j = i;
+		while (str[i] && (str[i] != ' ' && str[i] != '\t' && str[i] != '\n'))
+			i++;
+		if (i > j)
+		{
+			out[k] = malloc(sizeof(char) * (i - j) + 1);
+			ft_strncpy(out[k++], &str[j], (i - j));
+		}
+	}
+	out[k] = NULL;
+	return (out);
 }
