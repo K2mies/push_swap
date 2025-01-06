9f88a2aae723dcec3f67c6c9d7806a1c2034e5d5
Ross Hvidsten <rhvidste@student.hive.fi>
2025-01-06 10:22:04 +0200

    added test branch push_swap.c

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,30 +6,13 @@
 /*   By: rhvidste <rhvidste@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 13:04:29 by rhvidste          #+#    #+#             */
-/*   Updated: 2025/01/02 13:46:37 by rhvidste         ###   ########.fr       */
+/*   Updated: 2025/01/06 10:18:50 by rhvidste         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void	sort(t_stack_node *a, t_stack_node *b)
-{
-	if (stack_sorted(a) == 0)
-	{
-		if (stack_len(a) == 2)
-		{
-			sa(&a, 1);
-		}
-		else if (stack_len(a) == 3)
-		{
-			sort_three(&a);
-		}
-		else
-		{
-			sort_stacks(&a, &b);
-		}
-	}
-}
+void	ft_print_list(t_stack_node *list);
 
 int	main(int argc, char **argv)
 {
@@ -48,7 +31,45 @@ int	main(int argc, char **argv)
 	free(str);
 	init_stack_a(&a, split);
 	free_split(split);
-	sort(a, b);
+	//list output-----------------------------------
+	//ft_printf("linked list output before sort: \n");
+	//ft_print_list(a);
+	if (stack_sorted(a) == 0)
+	{
+		//ft_printf("stack not sorted \n");
+		if (stack_len(a) == 2)
+		{
+			sa(&a, 1);
+			//ft_printf("sorted stack:\n");
+			//ft_print_list(a);
+		}
+		else if (stack_len(a) == 3)
+		{
+			sort_three(&a);
+			//ft_printf("sorted stack:\n");
+			//ft_print_list(a);
+		}
+		else 
+		{
+			//ft_printf("larger stack sort needed:\n");
+			sort_stacks(&a, &b);
+			//ft_printf("sorted stack:\n");
+			//ft_print_list(a);
+		}
+	}
+	//else
+		//ft_printf("stack is sorted \n");
+	//ft_print_list(a);
 	free_stack(a);
 	free_stack(b);
+}
+
+//function to print output of list;
+void	ft_print_list(t_stack_node *list)
+{
+	while (list)
+	{
+		ft_printf("list[%d] = %d\n", list->index, list->nbr);
+		list = list->next;
+	}
 }
